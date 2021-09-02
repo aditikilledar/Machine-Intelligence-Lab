7261b85294f3ca1fdb88722072ad4b697807650e
aditikilledar <aditi.s.killedar@gmail.com>
2021-09-02 22:24:50 +0530

    changes

--- 2/PES1UG19CS031.py
+++ 2/PES1UG19CS031.py
@@ -5,6 +5,11 @@ Do not modify the given functions
 
 import numpy as np
 # from Collections import deque
+class Node:
+    def __init__(self, state, parent, cost):
+        self.cost = 0 # path cost till now
+        self.parent = [] # who its parents are
+        self.state = state # aka its value rn
 
 def A_star_Traversal(cost, heuristic, start_point, goals):
     """
@@ -36,116 +41,72 @@ def DFS_Traversal(cost, start_point, goals):
     """
     path = []
     # TODO
+    if not cost or not start_point or not goals:
+        return []
 
-    # trivial edge cases
-
-    # if not cost:
-    #     print("EDGE CASE cost empty")
-    #     exit
-
-    if not start_point or not goals or not cost:
-        print("EDGEEEEEEEEEEEEEEEEEEE")
-        exit
-
-    # frontier = deque()
+    # state, path cost, parent, action
     frontier = []
-    frontier.insert(0, start_point)
-    
-    success = 0
-    
-    npCost = np.array(cost)
-    expanded = [False] * npCost.shape[0]
-    
-    while True and success!=1:
-        print('frontier ', frontier)
-        if not frontier and success == 0:
-            print("frontier is emprty and we FAILED")
+    expanded = [False] * len(cost)
+    print(expanded)
 
-        ele = frontier.pop(0)
-        print(ele, ' popped')
+    ele = Node(start_point, [], 0)
+    frontier.insert(0, ele)
+    # print(ele.parent)
+    parents = {key: [] for key in range(1, len(cost))}
+    print(parents)
+    # print("frontier", frontier)
+    for each in frontier:
+                print("(state, parent)",each.state, each.parent)
 
-        if ele in goals:
-            print('Goal reached!')
-            success = 1
-            break
+    while True:
+        if len(frontier) == 0:
+            print("No solution")
+            return []
+
+        # remove node from top of stack
+        top = frontier.pop(0)
+        print("TOP: ",top.state)
+
+        value = top.state
+        print(value, 'expanded?', expanded[value])
+        if not expanded[value]:
+            # expand node if not already expanded
+            expanded[value] = True
+            # add largest ele in stack first
+            for j in reversed(range(1, len(cost))):
+                # print("j", j)
+                if (not expanded[j]) and (cost[value][j] != 0) and (cost[value][j] != -1): 
+                    frontier.insert(0, Node(j, [], 0))
+                    # parents[j].append(value)
+                    parents[j] = value
+                    print("parent added to",j,":", parents)
+
+            # print frontier
+            for each in frontier:
+                print("(state, parent)",each.state, each.parent, end = " ")
+
+
+        if top.state in goals:
+            print("Soln found!!!!!!!!!!!!!!")
+            break        
+
+    #PROBLEM: need to do smn so that it picks smaller parent first !!!!!!!!! time to sleep, good job Aditi :)
+
+    # goal reached
+    v = top.state
+    tempath = []
+    path.append(v)
+    # add parent of top into tempath until start_point is added, then reverse tempath and quit
+    while v != start_point:
+        path.append(parents[v])
+        print(path)
+        v=parents[v]
         
-        print('expanded ', expanded)
-        
-        if expanded[ele]:
-            # if already expanded
-            pass
-        else:
-            expanded[ele] = True
-            for j in range(1, npCost.shape[1]):
-                print("------------")
-                if (cost[ele][j] != -1) and (cost[ele][j] != 0):
-                    frontier.insert(0, j)
-                    print(j, 'is temp')
-                    print('frontier ', frontier)
-                    break
+    print(path)
+    path.reverse()
+    print(path)
 
-
-            # print(j, 'is temp')
-            # frontier.insert(0, j)
-            print('now expanding ', ele)
-            # path.append(ele)
-            print('expanded ', expanded)
-            
-    path = expanded 
-
-    # visited = [False] * npCost.shape[1]   
-    # pt=[]
-
-    # def dfs(visited, cost, node, pt):
-    #     print (node, end=" ")
-    #     visited[node]=True
-    #     for neigh in range(1, npCost.shape[1]):
-    #         if (cost[node][neigh] != 0) and (cost[node][neigh] != -1) and (not visited[neigh]):
-    #             dfs(visited, cost, neigh, pt) 
-
-    # dfs(visited, cost, start_point, pt)
+    if not path:
+        print("PATH Is EMPTY $$$$$$$$")
 
     return path
-
-# remove later   
-cost = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 5, 9, -1, 6, -1, -1, -1, -1, -1],
-            [0, -1, 0, 3, -1, -1, 9, -1, -1, -1, -1],
-            [0, -1, 2, 0, 1, -1, -1, -1, -1, -1, -1],
-            [0, 6, -1, -1, 0, -1, -1, 5, 7, -1, -1],
-            [0, -1, -1, -1, 2, 0, -1, -1, -1, 2, -1],
-            [0, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1],
-            [0, -1, -1, -1, -1, -1, -1, 0, -1, -1, -1],
-            [0, -1, -1, -1, -1, 2, -1, -1, 0, -1, 8],
-            [0, -1, -1, -1, -1, -1, -1, -1, -1, 0, 7],
-            [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0]]
-
-heuristic = [0, 5, 7, 3, 4, 6, 0, 0, 6, 5, 0]
-start = 1
-goals = [6, 7, 10]
-
-try:
-    if DFS_Traversal(cost,start, goals)==[1, 2, 3, 4, 7]:
-        print("Test Case 2 for DFS Traversal PASSED")
-    else:
-        print("Test Case 2 for DFS Traversal FAILED")
-except Exception as e:
-    print("Test Case 2 for DFS Traversal FAILED due to ",e)
-
-DFS_Traversal([],start, goals)
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
--- 2/retry.py
+++ 2/retry.py
@@ -111,39 +111,39 @@ def DFS_Traversal(cost, start_point, goals):
 
     return path
 
-# remove later   
-cost = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 5, 9, -1, 6, -1, -1, -1, -1, -1],
-            [0, -1, 0, 3, -1, -1, 9, -1, -1, -1, -1],
-            [0, -1, 2, 0, 1, -1, -1, -1, -1, -1, -1],
-            [0, 6, -1, -1, 0, -1, -1, 5, 7, -1, -1],
-            [0, 1, -1, -1, 2, 0, -1, -1, -1, 2, -1],
-            [0, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1],
-            [0, -1, -1, -1, -1, -1, -1, 0, -1, -1, -1],
-            [0, -1, -1, -1, -1, 2, -1, -1, 0, -1, 8],
-            [0, -1, -1, -1, -1, -1, -1, -1, -1, 0, 7],
-            [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0]]
+# # remove later   
+# cost = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#             [0, 0, 5, 9, -1, 6, -1, -1, -1, -1, -1],
+#             [0, -1, 0, 3, -1, -1, 9, -1, -1, -1, -1],
+#             [0, -1, 2, 0, 1, -1, -1, -1, -1, -1, -1],
+#             [0, 6, -1, -1, 0, -1, -1, 5, 7, -1, -1],
+#             [0, 1, -1, -1, 2, 0, -1, -1, -1, 2, -1],
+#             [0, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1],
+#             [0, -1, -1, -1, -1, -1, -1, 0, -1, -1, -1],
+#             [0, -1, -1, -1, -1, 2, -1, -1, 0, -1, 8],
+#             [0, -1, -1, -1, -1, -1, -1, -1, -1, 0, 7],
+#             [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0]]
 
-heuristic = [0, 5, 7, 3, 4, 6, 0, 0, 6, 5, 0]
-# start = 1
+# heuristic = [0, 5, 7, 3, 4, 6, 0, 0, 6, 5, 0]
+# # start = 1
 
-start = 4
-goals=[1]
+# start = 4
+# goals=[1]
 
 
-# goals = [6, 7, 10]
-# goals = [6]
+# # goals = [6, 7, 10]
+# # goals = [6]
 
-try:
-    if DFS_Traversal(cost,start, goals)==[1, 2, 3, 4, 7]:
-    # if DFS_Traversal(cost,start, goals)==[]:
-        print("Test Case 2 for DFS Traversal PASSED")
-    else:
-        print("Test Case 2 for DFS Traversal FAILED")
-except Exception as e:
-    print("Test Case 2 for DFS Traversal FAILED due to ",e)
+# try:
+#     if DFS_Traversal(cost,start, goals)==[1, 2, 3, 4, 7]:
+#     # if DFS_Traversal(cost,start, goals)==[]:
+#         print("Test Case 2 for DFS Traversal PASSED")
+#     else:
+#         print("Test Case 2 for DFS Traversal FAILED")
+# except Exception as e:
+#     print("Test Case 2 for DFS Traversal FAILED due to ",e)
 
-DFS_Traversal([],start, goals)
+# DFS_Traversal([],start, goals)
     
 
     
