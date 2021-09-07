012d34ab8bcaad36adf30d415f4dbcb8483c831c
aditikilledar <aditi.s.killedar@gmail.com>
2021-09-07 20:39:25 +0530

    week3

---
+++ 3/week3.py
@@ -0,0 +1,63 @@
+'''
+Assume df is a pandas dataframe object of the dataset given
+'''
+
+import numpy as np
+import pandas as pd
+import random
+
+
+'''Calculate the entropy of the enitre dataset'''
+# input:pandas_dataframe
+# output:int/float
+def get_entropy_of_dataset(df):
+    # TODO
+
+    # E(S) = -alog-blogb
+    # where, a = p/(n+p)
+    #    and b = n/(n+p)
+
+    return entropy
+
+
+'''Return avg_info of the attribute provided as parameter'''
+# input:pandas_dataframe,str   {i.e the column name ,ex: Temperature in the Play tennis dataset}
+# output:int/float
+def get_avg_info_of_attribute(df, attribute):
+    # TODO
+
+    # summation of 
+    for 
+
+
+    return avg_info
+
+
+'''Return Information Gain of the attribute provided as parameter'''
+# input:pandas_dataframe,str
+# output:int/float
+def get_information_gain(df, attribute):
+    # TODO
+
+    # attr_in_df = dataset with only that column and the results column
+    attr_in_df = 
+
+    attr_entropy = get_entropy_of_dataset()
+
+
+    return information_gain
+
+
+
+
+#input: pandas_dataframe
+#output: ({dict},'str')
+def get_selected_attribute(df):
+    '''
+    Return a tuple with the first element as a dictionary which has IG of all columns 
+    and the second element as a string with the name of the column selected
+
+    example : ({'A':0.123,'B':0.768,'C':1.23} , 'C')
+    '''
+    # TODO
+    pass
